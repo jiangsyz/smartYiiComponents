948ac228baea3b0f115566e3741c4a2656f6589f
jiangsyz <305460883@qq.com>
2018-04-10 20:01:04 +0800

    smartPagination增加findBySql的getData方法

--- smartPagination/smartPagination.php
+++ smartPagination/smartPagination.php
@@ -26,4 +26,29 @@ class smartPagination extends Component{
 		//返回
 		return array('totalCount'=>$totalCount,'totalPageNum'=>$totalPageNum,'pageSize'=>$pageSize,'pageNum'=>$pageNum,'objs'=>$objs);
 	}
+	//====================================================
+	public function getDataBySql($className,$sql,$pageSize,$pageNum){
+		//转换
+		$pageSize=(int)$pageSize;
+		$pageNum=(int)$pageNum;
+		//校验
+		if($pageSize<1) throw new SmartException("error pageSize");
+		if($pageNum<1) throw new SmartException("error pageNum");
+		//获取query
+		$query=$className::findBySql($sql);
+		//获取总数
+		$totalCount=(int)$query->count();
+		//获取总页数
+		$totalPageNum=ceil($totalCount/$pageSize);
+		//确定当前页数
+		$pageNum=$pageNum>$totalPageNum?$totalPageNum:$pageNum;
+		//确定起始记录
+		$start=($pageNum-1)*$pageSize;
+		//更新sql
+		$sql.=" LIMIT {$start},{$pageSize}";
+		//获取数据
+		$objs=$className::findBySql($sql)->all();
+		//返回
+		return array('totalCount'=>$totalCount,'totalPageNum'=>$totalPageNum,'pageSize'=>$pageSize,'pageNum'=>$pageNum,'objs'=>$objs);
+	}
 }

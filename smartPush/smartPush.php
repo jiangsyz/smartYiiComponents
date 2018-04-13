a04f6d3207878697d588b340ff50fccc915ff084
jiangsyz <305460883@qq.com>
2018-04-13 15:31:26 +0800

    smartPush

--- smartPush/smartPush.php
+++ smartPush/smartPush.php
@@ -20,19 +20,40 @@ class smartPush extends Component{
 	}
 	//========================================
 	//点对点推送
-	public function pushByRegistrationId($registrationId,$msg){
+	public function pushByRegistrationId($appType,$registrationId,$msg){
 		if(!$registrationId) throw new SmartException("miss registrationId");
 		if(!$msg) throw new SmartException("miss msg");
 		if(!isset($msg['title'])) throw new SmartException("msg miss title");
 		if(!isset($msg['msg_content'])) throw new SmartException("msg miss msg_content");
 		if(!isset($msg['content_type'])) throw new SmartException("msg miss content_type");
 		if(!isset($msg['extras'])) throw new SmartException("msg miss extras");
+		//推到安卓
+		if($appType=='Android') $this->pushByRegistrationIdToAndroid($registrationId,$msg);
+		//推到苹果
+		elseif($appType=='Ios') $this->pushByRegistrationIdToIos($registrationId,$msg);
+		//异常渠道
+		else throw new SmartException("error appType");
+	}
+	//========================================
+	//点对点推送至安卓
+	public function pushByRegistrationIdToAndroid($registrationId,$msg){
 		$this->client
 				->push()
-				->setPlatform(array('ios','android'))
+				->setPlatform(array('android'))
 				->addRegistrationId($registrationId)
-				->androidNotification('notice',array('extras'=>$msg['extras']))
-				->message('notice',$msg)
+				->androidNotification($msg['title'],array('extras'=>$msg['extras']))
+				->message($msg['title'],$msg)
+				->send();
+	}
+	//========================================
+	//点对点推送至苹果
+	public function pushByRegistrationIdToIos($registrationId,$msg){
+		$this->client
+				->push()
+				->setPlatform(array('ios'))
+				->addRegistrationId($registrationId)
+				->iosNotification($msg['title'],array('extras'=>$msg['extras']))
+				->message($msg['title'],$msg)
 				->send();
 	}
 }

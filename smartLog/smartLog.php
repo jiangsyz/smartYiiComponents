8413084236dcc24097a15501a303310e58bf89b6
jiangsyz <305460883@qq.com>
2018-06-04 11:34:02 +0800

    smartLog将HTTP_REQUEST_LOG和HTTP_RESPONSE_LOG整合为HTTP_LOG

--- smartLog/smartLog.php
+++ smartLog/smartLog.php
@@ -9,11 +9,10 @@ class smartLog extends smartComponentsNeedDb{
 	//日志开启等级
 	public $level=0;
 	//日志类型
-	const HTTP_REQUEST_LOG=1;//http请求日志
-	const HTTP_RESPONSE_LOG=2;//http应答日志
-	const CALL_API_LOG=4;//调用api日志
-	const EXCEPTION_LOG=8;//异常日志
-	const DEBUG_LOG=16;//调试日志
+	const HTTP_LOG=1;//http请求日志
+	const CALL_API_LOG=2;//调用api日志
+	const EXCEPTION_LOG=4;//异常日志
+	const DEBUG_LOG=8;//调试日志
 	//====================================================
 	private function log($logType,$data){
 		//该类型在配置中被关闭
@@ -24,12 +23,11 @@ class smartLog extends smartComponentsNeedDb{
 		$logRecord['logType']=$logType;
 		$logRecord['data']=$data;
 		$logRecord['time']=time();
-		smartLogRecord::addObj($logRecord);
+		return smartLogRecord::addObj($logRecord);
 	}
 	//====================================================
-	public function httpRequestLog($data){$this->log(self::HTTP_REQUEST_LOG,$data);}
-	public function httpResponseLog($data){$this->log(self::HTTP_RESPONSE_LOG,$data);}
-	public function callApiLog($data){$this->log(self::CALL_API_LOG,$data);}
-	public function exceptionLog($data){$this->log(self::EXCEPTION_LOG,$data);}
-	public function debugLog($data){$this->log(self::DEBUG_LOG,$data);}
+	public function httpLog($data){return $this->log(self::HTTP_LOG,$data);}
+	public function callApiLog($data){return $this->log(self::CALL_API_LOG,$data);}
+	public function exceptionLog($data){return $this->log(self::EXCEPTION_LOG,$data);}
+	public function debugLog($data){return $this->log(self::DEBUG_LOG,$data);}
 }

dcd20805a26fe33fc855fb3ce985479c864af74c
jiangsyz <305460883@qq.com>
2018-06-08 16:10:51 +0800

    smartWechat/smartWechat.php修改，将appid和appSecret作为方法传参数，并增加getAccessToken方法

--- smartWechat/smartWechat.php
+++ smartWechat/smartWechat.php
@@ -5,13 +5,9 @@ use Yii;
 use yii\base\Component;
 use yii\base\SmartException;
 class smartWechat extends Component{
-	//公众号信息
-	public $appID=false;
-	public $appSecret=false;
-	//========================================
 	//通过jscode获取sessionKey和openid
-	public function jscode2session($jscode){
-		$uri="https://api.weixin.qq.com/sns/jscode2session?appid={$this->appID}&secret={$this->appSecret}&js_code={$jscode}&grant_type=authorization_code";
+	public function jscode2session($appId,$appSecret,$jscode){
+		$uri="https://api.weixin.qq.com/sns/jscode2session?appid={$appId}&secret={$appSecret}&js_code={$jscode}&grant_type=authorization_code";
 		//调用接口
 		$response=Yii::$app->smartApi->get($uri,array(CURLOPT_SSL_VERIFYPEER=>false));
 		//处理数据
@@ -34,4 +30,16 @@ class smartWechat extends Component{
 		return $data;
 	}
 	//========================================
+	//获取accessToken
+	public function getAccessToken($appId,$appSecret){
+		$uri="https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid={$appId}&secret={$appSecret}";
+		//调用接口
+		$response=Yii::$app->smartApi->get($uri,array(CURLOPT_SSL_VERIFYPEER=>false));
+		//处理数据
+		$response=json_decode($response['response'],true);
+		if(!isset($response['access_token'])) throw new SmartException("获取accessToken失败",-2);
+		if(!isset($response['expires_in'])) throw new SmartException("获取expiresIn失败",-2);
+		//返回数据
+		return array('accessToken'=>$response['access_token'],'expiresIn'=>$response['expires_in']);
+	}
 }

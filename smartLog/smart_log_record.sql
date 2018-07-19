9dfa9076df2a47ba714649b76656249089bf75c3
jiangsyz <305460883@qq.com>
2018-07-19 16:02:08 +0800

    smartLog表结构增加索引

--- smartLog/smart_log_record.sql
+++ smartLog/smart_log_record.sql
@@ -1,20 +1,16 @@
 -- phpMyAdmin SQL Dump
--- version 4.3.13.2
--- http://www.phpmyadmin.net
+-- version 4.8.2
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: 2018-06-05 15:14:57
+-- Host: 120.132.20.205
+-- Generation Time: 2018-07-19 15:55:40
 -- 服务器版本： 5.5.58-log
 -- PHP Version: 5.6.13
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
 
 --
 -- Database: `smart_store_log`
@@ -26,7 +22,7 @@ SET time_zone = "+00:00";
 -- 表的结构 `smart_log_record`
 --
 
-CREATE TABLE IF NOT EXISTS `smart_log_record` (
+CREATE TABLE `smart_log_record` (
   `id` int(10) NOT NULL COMMENT '主键',
   `runningId` varchar(200) COLLATE utf8_bin NOT NULL COMMENT '本次php脚本运行的唯一编号',
   `controllerId` varchar(200) COLLATE utf8_bin NOT NULL COMMENT 'controllerId',
@@ -34,7 +30,7 @@ CREATE TABLE IF NOT EXISTS `smart_log_record` (
   `logType` int(10) NOT NULL COMMENT '日志类型',
   `data` text COLLATE utf8_bin NOT NULL COMMENT '日志内容',
   `time` int(10) NOT NULL COMMENT '日志记录时间戳'
-) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='smartLog日志';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='smartLog日志';
 
 --
 -- Indexes for dumped tables
@@ -44,17 +40,16 @@ CREATE TABLE IF NOT EXISTS `smart_log_record` (
 -- Indexes for table `smart_log_record`
 --
 ALTER TABLE `smart_log_record`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `runningId` (`runningId`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- 在导出的表使用AUTO_INCREMENT
 --
 
 --
--- AUTO_INCREMENT for table `smart_log_record`
+-- 使用表AUTO_INCREMENT `smart_log_record`
 --
 ALTER TABLE `smart_log_record`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT COMMENT '主键',AUTO_INCREMENT=0;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT COMMENT '主键';
+COMMIT;
--- smartLog/smart_log_record_bak.sql
+++ smartLog/smart_log_record_bak.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 120.132.20.205
--- Generation Time: 2018-07-11 08:55:01
+-- Generation Time: 2018-07-19 15:59:53
 -- 服务器版本： 5.5.58-log
 -- PHP Version: 5.6.13
 
@@ -11,12 +11,6 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `smart_store_log`
@@ -46,7 +40,8 @@ CREATE TABLE `smart_log_record_bak` (
 -- Indexes for table `smart_log_record_bak`
 --
 ALTER TABLE `smart_log_record_bak`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `runningId` (`runningId`);
 
 --
 -- 在导出的表使用AUTO_INCREMENT
@@ -58,7 +53,3 @@ ALTER TABLE `smart_log_record_bak`
 ALTER TABLE `smart_log_record_bak`
   MODIFY `id` int(10) NOT NULL AUTO_INCREMENT COMMENT '主键';
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
